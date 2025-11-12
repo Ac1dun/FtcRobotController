0c11e843b85795134426860816eb17e93a63f061
Ac1dun <acid4n@gmail.com>
2025-11-11 21:43:41 -0600

    Create Varible.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Varible.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Varible extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 23537;
+        double motorSpeed = 3.14;
+        boolean clawClosed = true;
+        String teamName = "itkan";
+        int motorAngle = 10;
+
+        telemetry.addData("TeamNumber", teamNumber);
+        telemetry.addData("motorSpeed", motorSpeed);
+        telemetry.addData("clawClosed", clawClosed);
+        telemetry.addData("Name", teamName);
+        telemetry.addData("Motor angle", motorAngle);
+
+
+    }
+
+    @Override
+    public void loop() {
+        /*
+
+         */
+    }
+}
